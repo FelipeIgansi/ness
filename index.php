0e01134e7067ecbd11dc984702a7a71c0584914b
Felipe <felipeigansi2009@hotmail.com>
2019-11-26 22:58:55 -0300

    version 17

--- index.php
+++ index.php
@@ -298,7 +298,7 @@
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Contas</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Adicionar Conta</a>
+            <a href="adicionarConta.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Adicionar Conta</a>
           </div>
 
 
