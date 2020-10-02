db9549946597ac70a4be3ef1cbf06a669173ee2d
FelipeIgansi <lipeigansi@gmail.com>
2020-10-02 07:55:00 -0300

    CorrecaoNosSQLs_MelhoraNaSintaxeDeAdicionarConta

--- sql/proc_contas_insert.sql
+++ sql/proc_contas_insert.sql
@@ -1,18 +1,17 @@
+delimiter $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_contas_insert`(
-	parameter_nome varchar(50),
-    parameter_email varchar(50),
-    parameter_senha varchar(64),
-    parameter_url varchar(255),
-    parameter_tipoconta varchar(20),
-    parameter_fk_usuario int(11)
-
-
-)
+	`parameter_nome` varchar(50),
+    `parameter_email` varchar(50),
+    `parameter_senha` varchar(64),
+    `parameter_url` varchar(255),
+    `parameter_tipoconta` varchar(20),
+    `parameter_fk_usuario` int(11) 
+    )
 BEGIN
 	INSERT INTO `projetogestordesenhas`.`conta`
 	VALUES
-	(default, parameter_nome, parameter_email, parameter_senha, parameter_url, parameter_tipoconta, parameter_fk_usuario);
+	(`default`, `parameter_nome`, `parameter_email`, `parameter_senha`, `parameter_url`, `parameter_tipoconta`, `parameter_fk_usuario`);
 	
-    SELECT * FROM `projetogestordesenhas`.`conta` where idConta = last_insert_id();
+    SELECT * FROM `projetogestordesenhas`.`conta` where `idConta` = `last_insert_id()`;
 
-END
+END $$
--- sql/proc_usuarios_insert.sql
+++ sql/proc_usuarios_insert.sql
@@ -1,16 +1,18 @@
+Delimiter $$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_usuarios_insert`(
-    parameter_nome varchar(50),
-    parameter_email varchar(100),
-    parameter_senha varchar(64)
+    `parameter_nome` varchar(50),
+    `parameter_email` varchar(100),
+    `parameter_senha` varchar(64)
 
 )
 BEGIN
 
-	insert into usuario
-    values(default, parameter_nome, parameter_email,parameter_senha);
+	insert into `usuario`
+    values(`default`, `parameter_nome`, `parameter_email`,`parameter_senha`);
     
     
-    select * from usuario where idUsuario = last_insert_id();
+    select * from `usuario` where `idUsuario` = `last_insert_id()`;
 
 
-END
+END $$
